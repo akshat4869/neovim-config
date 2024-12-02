984d08aa970d5c79c2eea99c169329d9e45d4103
Akshat Singh <akshat>
2024-12-03 01:14:38 +0530

    Create inital neovim config

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("akshat.set")
+require("akshat.remap")
+require("akshat.lazy_init")
+
+print("Hello")
+
+
---
+++ lua/akshat/set.lua
@@ -0,0 +1,5 @@
+-- Set basic vim config
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = " "
